f078f02093fe98b44fee480cf2ca4b1b1ff97cdf
vinaytiwari <vinaytiwari264@gmail.com>
2019-03-29 16:21:22 +0530

    Added Entities and Data project , Installed  Entity Fremwork in Data , and Enabled Migrations

---
+++ DealDouble.Data/DealDoubleContext.cs
@@ -0,0 +1,19 @@
+﻿using DealDouble.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDouble.Data
+{
+    class DealDoubleContext : DbContext
+    {
+        public DealDoubleContext() : base("name=DealDoubleConnectionString")
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
